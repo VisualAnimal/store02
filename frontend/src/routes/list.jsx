82143dbb4641bd962401b2186e07ee0c0bcbb2b7
unknown <visualanimal@icloud.com>
2023-11-10 23:43:28 +0800

    后端添加order字段，前端清除注释代码

--- frontend/src/routes/list.jsx
+++ frontend/src/routes/list.jsx
@@ -1,8 +1,6 @@
 import { useQuery } from "@apollo/client"
 import { GET_BRAND_AND_PRODUCT } from "../serve/queries";
-// import './style.css'
-import { ProductCard, Sidebar, SidebarItem, Skeleton, Sticky } from 'react-vant';
-import { Location, Phone } from "@react-vant/icons"
+import { Skeleton, Sticky } from 'react-vant';
 import { useState } from "react"
 import { Menu } from "../components/menu";
 import { styled } from 'styled-components'
@@ -22,28 +20,14 @@ const Left = styled.div`
         background-color:var(--rv-background-color-light);
         width: 23%;
         height: 100vh;
-        /* overflow-y: scroll; */
-        /* overflow-y: hidden; */
-        /* overflow-y: auto; */
     `
 const Right = styled.div`
         width: 77%;
-        /* background-color: #fff111; */
     `
 export default function List() {
     const { loading, error, data } = useQuery(GET_BRAND_AND_PRODUCT)
     const [brand, setBrand] = useState(0)
     const [model, setModel] = useState('')
-    // console.log(model);
-
-    // if (loading) return <p>Loading...</p>;
-    // if (error) return <p>Error : {error.message}</p>;
-    // console.log(data);
-
-    const getMenuChecked = (val) => {
-        // console.log(val);
-        setBrand(val)
-    }
 
     const modelSelect = (model) => {
         console.log(model);
@@ -57,21 +41,18 @@ export default function List() {
                 <Left>
                     <Sticky offsetTop={80}>
                         {loading ? (<Skeleton rowHeight={40} row={10} />) : (
-                            <Menu props={{ data, getMenuChecked }} />
+                            <Menu props={{ data, setBrand }} />
                         )}
                     </Sticky>
                 </Left>
                 <Right>
                     {loading ? (<Skeleton row={10} rowHeight={80} />) : (
                         <div>
-                            <FilterBar props={{brand: data.brands[brand], setModel}}></FilterBar>
-                            <Card props={{brand:data.brands[brand], model}}></Card>
+                            <FilterBar props={{ brand: data.brands[brand], setModel }}></FilterBar>
+                            <Card props={{ brand: data.brands[brand], model }}></Card>
                         </div>
                     )}
                 </Right>
-                {/* <ul>{data.brands.map(brand => (
-                <li>{brand.name}</li>
-            ))}</ul> */}
             </Wrapper>
         </Container>
     )
